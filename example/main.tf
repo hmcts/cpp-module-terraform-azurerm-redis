13754bad2ce17e6a4513a96411624a8ad1984a20
Jyothi Balla <jyothi.balla@hmcts.net>
2023-05-04 12:58:41 +0100

    update config

--- example/main.tf
+++ example/main.tf
@@ -23,12 +23,7 @@ module "redis" {
   redis_server_settings = var.redis_server_settings
 
   redis_configuration = {
-    enable_authentication           = lookup(var.redis_configuration, "enable_authentication", true)
-    maxmemory_reserved              = lookup(var.redis_configuration, "maxmemory_reserved")
-    maxmemory_delta                 = lookup(var.redis_configuration, "maxmemory_delta")
-    maxmemory_policy                = lookup(var.redis_configuration, "maxmemory_policy")
-    maxfragmentationmemory_reserved = 200
-    notify_keyspace_events          = null
+    enable_authentication = lookup(var.redis_configuration, "enable_authentication", true)
   }
 
 }
